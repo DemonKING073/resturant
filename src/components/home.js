f6d696ad5d2962ff1276e09dc2e213983bb1007a
DemonKING073 <legendsnever7die@gmail.com>
2020-08-30 14:10:18 +0545

    first phase

---
+++ src/components/home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Home=()=>{
+    return(
+        <section className="section first">
+            <div className="quote">
+                LIVE LONGER WITH HEALTHY FOODS
+            </div>
+            <div className="text">
+                We believe in not only having delicious foods but nutritious foods that you can feel good about eating.
+            </div>
+        </section>
+    );
+}
+
+export default Home;
---
+++ src/components/nav.js
@@ -0,0 +1,28 @@
+import {Link} from 'react-router-dom';
+import React from 'react';
+
+const Nav=()=>{
+    return(
+        <nav>
+            <div className="logo-name">Himawari Dinner</div>
+            <div className="nav-links">
+                <ul>
+                    <Link to="/">
+                        <li>Home</li>
+                    </Link>
+                    <Link to="/menu">
+                        <li>Menu</li>
+                    </Link>
+                    <Link to="/contact">
+                        <li>Contact</li>
+                    </Link>
+                    <Link to="/about">
+                        <li>About</li>
+                    </Link>
+                </ul>
+            </div>
+        </nav>
+    );
+}
+
+export default Nav;
