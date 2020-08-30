405fa317173cdcbf6e0d3d7e0e6764968fd9dcce
DemonKING073 <legendsnever7die@gmail.com>
2020-08-30 21:17:05 +0545

    about to end homepage

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import Facebook from '../assets/Facebook.svg';
+import Twitter from '../assets/Twitter.svg';
+import Insta from '../assets/Instagram.svg';
 
 const Footer=()=>{
     return(
-        <div>
-            This is Footer
-        </div>
+        <footer>
+            <div className="logo-name small">Himawari Dinner</div>
+            <div className="footer-info">&copy;Dinner by Chris Thapa, Jhapa,Nepal</div>
+            
+            <div></div>
+            <div className="icon-div">
+                <img src={Facebook} alt="Facebook" />
+                <img src={Twitter} alt="Twitter" />
+                <img src={Insta} alt="Instagram" />
+            </div>
+        </footer>
     );
 }
 
--- src/components/home.js
+++ src/components/home.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 const Home=()=>{
     return(
+        <>
         <section className="section first">
             <div className="quote">
                 LIVE LONGER WITH HEALTHY FOODS
@@ -10,6 +11,31 @@ const Home=()=>{
                 We believe in not only having delicious foods but nutritious foods that you can feel good about eating.
             </div>
         </section>
+        <section className="section second">
+            <div className="text-area">
+                <div className="header">
+                    Order Online
+                </div>
+                <div className="order">
+                    Our resturant is just opened and it's coverage is just our region. Don't worry! Soon we'll cover more regions as soon as possible.
+                </div>
+                <div className="btn-div">
+                    <button>Order Now</button>
+                    <button>Menu</button>
+                </div>
+
+            </div>
+            <div className="fruit-background"></div>
+
+        </section>
+        <section className="section third"> 
+        <div className="header">
+            Our Specials
+        </div>
+
+        </section>
+
+        </>
     );
 }
 
