99f1178aee80df62b565ed896e7e532dff807956
DemonKING073 <legendsnever7die@gmail.com>
2020-09-01 13:17:59 +0545

    fast-food styling

---
+++ src/components/card.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+const Card=(props)=>{
+    if(props!==undefined)
+    {
+        
+        console.log(props);
+        return(
+        
+            <div className="card-menu">
+                <img src={props.img} alt={props.title} />
+                <h1 className="menu-title">{props.title}</h1>
+                <div className="card-bottom">
+                    <div >
+                        Price: $<span >{props.price}</span>
+                    </div>
+                    <div>
+                        <button className="cart-btn">Add to Cart</button>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+    
+    else
+    return(
+        <div>
+            Loading
+        </div>
+    );
+        
+    
+    
+}
+
+export default Card;
--- src/components/menu.js
+++ src/components/menu.js
@@ -1,7 +1,61 @@
 import React from 'react';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+import Card from './card';
+import Image1 from '../assets/VegieBalls.jpg';
+import Image2 from '../assets/chicken.jpg';
 
 
 const Menu=()=>{
+
+    const fastFoods=[
+        {
+            imgSrc:Image1,
+            title:'Vegie Rolls',
+            price:200,
+            desc:'',
+        },
+        {
+            imgSrc:Image2,
+            title:'Chicken',
+            price:250,
+            desc:'',
+        },
+        {
+            imgSrc:Image2,
+            title:'Chicken',
+            price:250,
+            desc:'',
+        },
+        {
+            imgSrc:Image2,
+            title:'Chicken',
+            price:250,
+            desc:'',
+        },
+        {
+            imgSrc:Image2,
+            title:'Chicken',
+            price:250,
+            desc:'',
+        },
+        {
+            imgSrc:Image2,
+            title:'Chicken',
+            price:250,
+            desc:'',
+        },
+    
+    ];
+    var settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 3,
+        slidesToScroll: 1
+      };
+
     return(
         <section className="section menu">
             <div className="short-width">
@@ -10,10 +64,18 @@ const Menu=()=>{
                 </div>
                 <div className="food-container">
                     <span className="food-title">Fast Foods</span>
-                    <span class="explore">See All</span>
+                    <span className="explore">See All</span>
                 </div>
                 <div className="food-lists">
-
+                    <Slider {...settings}>
+                    {fastFoods.map((item,index)=>{
+                        return(
+                            <>
+                            <Card key={index} title={item.title} img={item.imgSrc} price={item.price} desc={item.desc}/>
+                            </>
+                        );
+                    })}
+                    </Slider>
                 </div>
             </div>
         </section>
